a4de6c389be4e0a9ad60b001f99a1b272df647b8
Max Shaposhnik <mshaposhnik@codenvy.com>
2015-06-17 12:43:25 +0300

    Added setenv.bat;

--- assembly-sdk/src/assembly/bin/setenv.bat
+++ assembly-sdk/src/assembly/bin/setenv.bat
@@ -5,9 +5,9 @@
 @REM [2012] - [2013] Codenvy, S.A.
 @REM All Rights Reserved.
 @REM
-@REM NOTICE:  All information contained herein is, and remains
+@REM NOTICE: All information contained herein is, and remains
 @REM the property of Codenvy S.A. and its suppliers,
-@REM if any.  The intellectual and technical concepts contained
+@REM if any. The intellectual and technical concepts contained
 @REM herein are proprietary to Codenvy S.A.
 @REM and its suppliers and may be covered by U.S. and Foreign Patents,
 @REM patents in process, and are protected by trade secret or copyright law.
@@ -15,15 +15,29 @@
 @REM is strictly forbidden unless prior written permission is obtained
 @REM from Codenvy S.A..
 @REM
-
 @echo off
+if "%CODENVY_LOCAL_CONF_DIR%"==""   (
+   echo Need to set CODENVY_LOCAL_CONF_DIR
+   echo Press enter to exit...
+   pause >nul
+   exit
+)
 
-set IDE_OPTS="-Dorg.exoplatform.logreader.logpath=%CATALINA_HOME%/logs/logreader"
-set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-set CODENVY_CONFIG_OPTS="-Xshare:auto -Xms512m -Xmx1024m -XX:MaxPermSize=256m"
-set JAVA_OPTS=%LOG_OPTS% %CODENVY_CONFIG_OPTS% %IDE_OPTS%
-set SERVER_PORT=8080
-set CLASSPATH=%CATALINA_HOME%/conf/;%CATALINA_HOME%/lib/jul-to-slf4j.jar;%CATALINA_HOME%/lib/slf4j-api.jar;%CATALINA_HOME%/lib/logback-classic.jar;%CATALINA_HOME%/lib/logback-core.jar;%JAVA_HOME%/lib/tools.jar
+if "%JAVA_OPTS%"=="" (set JAVA_OPTS=-Xms256m -Xmx1048m -XX:MaxPermSize=256m -server)
+
+if "%CODENVY_LOGS_DIR%"=="" (set CODENVY_LOGS_DIR=%CATALINA_HOME%\logs)
+
+if "%JPDA_ADDRESS%"=="" (set JPDA_ADDRESS=8000)
+
+if "%SERVER_PORT%"=="" (set SERVER_PORT=8080)
+
+if "%CATALINA_OPTS%"=="" (set CATALINA_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcodenvy.local.conf.dir=%CODENVY_LOCAL_CONF_DIR%)
+
+if "%CLASSPATH%"=="" (set CLASSPATH=%CATALINA_HOME%\conf\;%JAVA_HOME%\lib\tools.jar)
+
+set LOG_OPTS=-Dcodenvy.logs.dir=%CODENVY_LOGS_DIR%
+
+set JAVA_OPTS=%JAVA_OPTS% %LOG_OPTS%
 echo "======="
 echo %JAVA_OPTS%
 echo "======="
